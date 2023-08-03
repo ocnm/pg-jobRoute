7613bdfca2d410ff3cc2b25840f1d74b075af7ca
ocnm <95259793+ocnm@users.noreply.github.com>
2023-08-03 02:45:38 -0600

    fixing LoaderScreen for next.js

--- components/LoaderScreen.jsx
+++ components/LoaderScreen.jsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 const LoaderScreen = () => {
   return <div className='fixed inset-0 flex items-center justify-center'>
     <Image
+      width={400}
+      height={400}
       src='/loader.gif'
       alt='loader'
       className= 'max-w-full max-h-full rounded-full scale-100'

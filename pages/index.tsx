86091dfdad94f59941fc9479c96b8a6a883b695e
ocnm <95259793+ocnm@users.noreply.github.com>
2023-08-03 14:28:00 -0600

    Updating layout and logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import { Inter } from 'next/font/google'
 import { useState } from 'react';
 import LoaderScreen from '@/components/LoaderScreen';
-// import ReCAPTCHA from 'react-google-recaptcha';
+import ReCAPTCHA, { ReCAPTCHAProps } from "react-google-recaptcha";
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('');
 
 const inter = Inter({ subsets: ['latin'] })
 
 
 const Home = () => {
   const [loader, setLoader] = useState(false);
+  const [captcha, setCaptcha] = useState(false);
   const [profession, setProfession] = useState('Front end developer');
   const [skill, setSkill] = useState('Angular');
   const [data, setData] = useState<any>([]);
@@ -15,9 +19,8 @@ const Home = () => {
   const s = {
     input: 'p-5 m-3 rounded-md shadow-2xl blur-xs text-xl',
     submitBtn: 'border-solid border-2 rounded-md p-3 m-3 text-xl hover:rounded-full active:bg-white active:text-blue-700',
-    listBtn: 'border-2 border-blue-500 m-1 rounded-lg p-3 text-xl text-black-500 hover:shadow-xl',
+    listBtn: 'border-2 border-black bg-white m-1 rounded-lg p-3 text-xl text-black-500 hover:shadow-xl hover:bg-blue-300',
   }
-
 
   const handleRequest = async (data: any, l: number) => {
     setLoader(true);
@@ -50,12 +53,42 @@ const Home = () => {
   }
 
   const handleSubmit = async () => {
-    handleRequest({ p: profession, s: skill }, 1);
+    if (captcha) {
+      handleRequest({ p: profession, s: skill }, 1);
+      setCaptcha(false);
+    } else alert('Please, pass captcha');
+
   };
 
   const handleNode = async (title: any) => {
-    const topic = `${title.title} - ${title.desc}`
-    handleRequest({ topic }, 2);
+    if (captcha) {
+      const topic = `${title.title} - ${title.desc}`
+      handleRequest({ topic }, 2);
+      setCaptcha(false);
+    } else alert('Please, pass captcha');
+  }
+
+  const handleCaptcha: ReCAPTCHAProps['onChange'] = async (value: string) => {
+    try {
+      const res = await fetch('/api/verify', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ captcha: value }),
+      });
+
+      const success = await res.json();
+
+      if (success) {
+        setCaptcha(true);
+      } else {
+        setCaptcha(false)
+        alert('captcha failed');
+      }
+    } catch (err) {
+      alert(`An error occurred while verifying the reCAPTCHA:\n${err}`);
+    }
   }
 
   const firstNode = (response: any) => response.map((point: any, i: any) => (
@@ -71,22 +104,29 @@ const Home = () => {
     <div className='flex flex-col items-center justify-start w-screen h-screen flex-nowrap gap-4 bg-gradient'>
       {!loader &&
         <>
-          <input
-            className={s.input}
-            onChange={(e) => handleChange(e, setProfession)}
-            value={profession} placeholder='type the profession'
-            type="text"
-          />
+          <div>
+            <input
+              className={s.input}
+              onChange={(e) => handleChange(e, setProfession)}
+              value={profession} placeholder='type the profession'
+              type='text'
+            />
 
-          <input
-            className={s.input}
-            onChange={(e) => handleChange(e, setSkill)}
-            value={skill}
-            placeholder='type the skill'
-            type="text"
-          />
+            <input
+              className={s.input}
+              onChange={(e) => handleChange(e, setSkill)}
+              value={skill}
+              placeholder='type the skill'
+              type='text'
+            />
+          </div>
 
-          <button className={s.submitBtn} onClick={handleSubmit}>Generate</button>
+          <ReCAPTCHA sitekey='6Lcaz5kmAAAAAMghKz-e4BDJRKOLd0ZDncPUum5Q'
+            onChange={handleCaptcha} />
+          {captcha &&
+            <button className={s.submitBtn} onClick={handleSubmit}>Generate</button>
+          }
+
           <div className='flex flex-col justify-between'>
             {data && firstNode(data)}
           </div>
